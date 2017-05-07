c3cf4b8318e2bdc219ab30f0600d060c58383b69
IrynaIeremeichuk <ieremeichukiryna@gmail.com>
2017-05-07 11:22:12 -0700

    New functions are added (Square area, Rectangle area)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-	
-          System.out.println("Hello, world!");
-        }
+    hello("world");
+    hello("user");
+    hello("Iryna");
+
+    double l = 5;
+    System.out.println("Square = " + area(l));
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Rectangle = " + area(a,b));
+  }
+
+  public static void hello(String s){
+    System.out.println("Hello, " + s + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a*b;
+  }
 }
